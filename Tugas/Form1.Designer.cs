e5a29812fa10752711f87fa978a4d3bab7062c6b
resturino <134687297+resturino@users.noreply.github.com>
2023-05-26 15:20:55 +0700

    Add files via upload

---
+++ Tugas/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Tugas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpMode = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.grpBangun = new System.Windows.Forms.GroupBox();
+            this.rdoNIM = new System.Windows.Forms.RadioButton();
+            this.rdoSegi = new System.Windows.Forms.RadioButton();
+            this.rdoPersegi = new System.Windows.Forms.RadioButton();
+            this.grpSkor = new System.Windows.Forms.GroupBox();
+            this.grpWarna = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rdoBiru = new System.Windows.Forms.RadioButton();
+            this.rdoPutih = new System.Windows.Forms.RadioButton();
+            this.rdoKuning = new System.Windows.Forms.RadioButton();
+            this.rdoMerah = new System.Windows.Forms.RadioButton();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlBlackboard = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.grpMode.SuspendLayout();
+            this.grpBangun.SuspendLayout();
+            this.grpWarna.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpMode
+            // 
+            this.grpMode.Controls.Add(this.checkBox2);
+            this.grpMode.Controls.Add(this.checkBox1);
+            this.grpMode.Location = new System.Drawing.Point(14, 17);
+            this.grpMode.Name = "grpMode";
+            this.grpMode.Size = new System.Drawing.Size(307, 143);
+            this.grpMode.TabIndex = 0;
+            this.grpMode.TabStop = false;
+            this.grpMode.Text = "Mode";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(43, 88);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(66, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Game";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(43, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Visualisasi";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // grpBangun
+            // 
+            this.grpBangun.Controls.Add(this.rdoNIM);
+            this.grpBangun.Controls.Add(this.rdoSegi);
+            this.grpBangun.Controls.Add(this.rdoPersegi);
+            this.grpBangun.Location = new System.Drawing.Point(17, 188);
+            this.grpBangun.Name = "grpBangun";
+            this.grpBangun.Size = new System.Drawing.Size(303, 133);
+            this.grpBangun.TabIndex = 1;
+            this.grpBangun.TabStop = false;
+            this.grpBangun.Text = "Bangun Datar";
+            this.grpBangun.Enter += new System.EventHandler(this.grpBangun_Enter);
+            // 
+            // rdoNIM
+            // 
+            this.rdoNIM.AutoSize = true;
+            this.rdoNIM.Location = new System.Drawing.Point(30, 97);
+            this.rdoNIM.Name = "rdoNIM";
+            this.rdoNIM.Size = new System.Drawing.Size(96, 20);
+            this.rdoNIM.TabIndex = 2;
+            this.rdoNIM.TabStop = true;
+            this.rdoNIM.Text = "3. Your NIM";
+            this.rdoNIM.UseVisualStyleBackColor = true;
+            this.rdoNIM.CheckedChanged += new System.EventHandler(this.rdoNIM_CheckedChanged);
+            // 
+            // rdoSegi
+            // 
+            this.rdoSegi.AutoSize = true;
+            this.rdoSegi.Location = new System.Drawing.Point(30, 61);
+            this.rdoSegi.Name = "rdoSegi";
+            this.rdoSegi.Size = new System.Drawing.Size(124, 20);
+            this.rdoSegi.TabIndex = 1;
+            this.rdoSegi.TabStop = true;
+            this.rdoSegi.Text = "2. Segi Delapan";
+            this.rdoSegi.UseVisualStyleBackColor = true;
+            this.rdoSegi.CheckedChanged += new System.EventHandler(this.rdoSegi_CheckedChanged);
+            // 
+            // rdoPersegi
+            // 
+            this.rdoPersegi.AutoSize = true;
+            this.rdoPersegi.Location = new System.Drawing.Point(30, 22);
+            this.rdoPersegi.Name = "rdoPersegi";
+            this.rdoPersegi.Size = new System.Drawing.Size(88, 20);
+            this.rdoPersegi.TabIndex = 0;
+            this.rdoPersegi.TabStop = true;
+            this.rdoPersegi.Text = "1. Persegi";
+            this.rdoPersegi.UseVisualStyleBackColor = true;
+            this.rdoPersegi.CheckedChanged += new System.EventHandler(this.rdoPersegi_CheckedChanged);
+            // 
+            // grpSkor
+            // 
+            this.grpSkor.Location = new System.Drawing.Point(16, 344);
+            this.grpSkor.Name = "grpSkor";
+            this.grpSkor.Size = new System.Drawing.Size(304, 87);
+            this.grpSkor.TabIndex = 2;
+            this.grpSkor.TabStop = false;
+            this.grpSkor.Text = "Skor";
+            this.grpSkor.Enter += new System.EventHandler(this.grpSkor_Enter);
+            // 
+            // grpWarna
+            // 
+            this.grpWarna.BackColor = System.Drawing.SystemColors.Menu;
+            this.grpWarna.Controls.Add(this.textBox3);
+            this.grpWarna.Controls.Add(this.textBox2);
+            this.grpWarna.Controls.Add(this.textBox1);
+            this.grpWarna.Controls.Add(this.richTextBox1);
+            this.grpWarna.Controls.Add(this.rdoBiru);
+            this.grpWarna.Controls.Add(this.rdoPutih);
+            this.grpWarna.Controls.Add(this.rdoKuning);
+            this.grpWarna.Controls.Add(this.rdoMerah);
+            this.grpWarna.Location = new System.Drawing.Point(347, 326);
+            this.grpWarna.Name = "grpWarna";
+            this.grpWarna.Size = new System.Drawing.Size(431, 61);
+            this.grpWarna.TabIndex = 3;
+            this.grpWarna.TabStop = false;
+            this.grpWarna.Text = "Warna";
+            this.grpWarna.Enter += new System.EventHandler(this.grpWarna_Enter);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(354, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(60, 15);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Putih";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(247, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(80, 15);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Kuning";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(136, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(82, 15);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Biru";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(28, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(81, 20);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "Merah";
+            // 
+            // rdoBiru
+            // 
+            this.rdoBiru.AutoSize = true;
+            this.rdoBiru.Location = new System.Drawing.Point(115, 26);
+            this.rdoBiru.Name = "rdoBiru";
+            this.rdoBiru.Size = new System.Drawing.Size(51, 20);
+            this.rdoBiru.TabIndex = 3;
+            this.rdoBiru.TabStop = true;
+            this.rdoBiru.Text = "Biru";
+            this.rdoBiru.UseVisualStyleBackColor = true;
+            this.rdoBiru.CheckedChanged += new System.EventHandler(this.rdoBiru_CheckedChanged);
+            // 
+            // rdoPutih
+            // 
+            this.rdoPutih.AutoSize = true;
+            this.rdoPutih.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rdoPutih.Location = new System.Drawing.Point(333, 26);
+            this.rdoPutih.Name = "rdoPutih";
+            this.rdoPutih.Size = new System.Drawing.Size(57, 20);
+            this.rdoPutih.TabIndex = 2;
+            this.rdoPutih.TabStop = true;
+            this.rdoPutih.Text = "Putih";
+            this.rdoPutih.UseVisualStyleBackColor = true;
+            this.rdoPutih.CheckedChanged += new System.EventHandler(this.rdoPutih_CheckedChanged);
+            // 
+            // rdoKuning
+            // 
+            this.rdoKuning.AutoSize = true;
+            this.rdoKuning.Location = new System.Drawing.Point(224, 26);
+            this.rdoKuning.Name = "rdoKuning";
+            this.rdoKuning.Size = new System.Drawing.Size(68, 20);
+            this.rdoKuning.TabIndex = 1;
+            this.rdoKuning.TabStop = true;
+            this.rdoKuning.Text = "Kuning";
+            this.rdoKuning.UseVisualStyleBackColor = true;
+            this.rdoKuning.CheckedChanged += new System.EventHandler(this.rdoKuning_CheckedChanged);
+            // 
+            // rdoMerah
+            // 
+            this.rdoMerah.AutoSize = true;
+            this.rdoMerah.Location = new System.Drawing.Point(6, 28);
+            this.rdoMerah.Name = "rdoMerah";
+            this.rdoMerah.Size = new System.Drawing.Size(66, 20);
+            this.rdoMerah.TabIndex = 0;
+            this.rdoMerah.TabStop = true;
+            this.rdoMerah.Text = "Merah";
+            this.rdoMerah.UseVisualStyleBackColor = true;
+            this.rdoMerah.CheckedChanged += new System.EventHandler(this.rdoMerah_CheckedChanged);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(347, 398);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(109, 33);
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(511, 398);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(109, 33);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(668, 398);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 33);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pnlBlackboard
+            // 
+            this.pnlBlackboard.BackColor = System.Drawing.Color.Black;
+            this.pnlBlackboard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlBlackboard.Location = new System.Drawing.Point(347, 17);
+            this.pnlBlackboard.Name = "pnlBlackboard";
+            this.pnlBlackboard.Size = new System.Drawing.Size(431, 303);
+            this.pnlBlackboard.TabIndex = 7;
+            this.pnlBlackboard.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBlackboard_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlBlackboard);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.grpWarna);
+            this.Controls.Add(this.grpSkor);
+            this.Controls.Add(this.grpBangun);
+            this.Controls.Add(this.grpMode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpMode.ResumeLayout(false);
+            this.grpMode.PerformLayout();
+            this.grpBangun.ResumeLayout(false);
+            this.grpBangun.PerformLayout();
+            this.grpWarna.ResumeLayout(false);
+            this.grpWarna.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMode;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox grpBangun;
+        private System.Windows.Forms.GroupBox grpSkor;
+        private System.Windows.Forms.GroupBox grpWarna;
+        private System.Windows.Forms.RadioButton rdoBiru;
+        private System.Windows.Forms.RadioButton rdoPutih;
+        private System.Windows.Forms.RadioButton rdoKuning;
+        private System.Windows.Forms.RadioButton rdoMerah;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RadioButton rdoNIM;
+        private System.Windows.Forms.RadioButton rdoSegi;
+        private System.Windows.Forms.RadioButton rdoPersegi;
+        private System.Windows.Forms.Panel pnlBlackboard;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Tugas/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Tugas
+{
+    public partial class Form1 : Form
+    {
+        bool mousePress;
+        int xLast;
+        int yLast;
+        Graphics myGraphics;
+        Pen myPen;
+        private Point[] segiPoints; 
+        int rectX;
+        int rectY;
+        int rectWidth;
+        int rectHeight;
+        bool drawRectangle;
+        bool showNIM = false;
+        bool allowPaint = false;
+        private int score;
+        private int generatedNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnlBlackboard.BackgroundImage = new Bitmap(pnlBlackboard.Width, pnlBlackboard.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            myGraphics = Graphics.FromImage(pnlBlackboard.BackgroundImage);
+            myGraphics = pnlBlackboard.CreateGraphics();
+            myPen = new Pen(Color.White, 1);
+            rdoMerah.ForeColor = Color.Red;
+            rdoBiru.ForeColor = Color.Blue;
+            rdoKuning.ForeColor = Color.Yellow;
+            rdoPutih.ForeColor = Color.White;
+            rdoPutih.Checked = true;
+            mousePress = false;
+            drawRectangle = false;
+            showNIM = false;
+        }
+        private void pnlBlackboard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousePress)
+            {
+                pnlBlackboard.Refresh();
+                myGraphics.DrawLine(myPen, xLast, yLast, e.X, e.Y);
+                xLast = e.X;
+                yLast = e.Y;
+            }
+        }
+        private void pnlBlackboard_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                pnlBlackboard.Refresh();
+                myGraphics.DrawLine(myPen, xLast, yLast, e.X, e.Y);
+                mousePress = false;
+            }
+        }
+        private void pnlBlackboard_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mousePress = true;
+                xLast = e.X;
+                yLast = e.Y;
+            }
+        }
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                if (generatedNumber != 0)
+                {
+                    if ((rdoPersegi.Checked && generatedNumber == 1) ||
+                        (rdoSegi.Checked && generatedNumber == 2) ||
+                        (rdoNIM.Checked && generatedNumber == 3))
+                    {
+                        score = 100;
+                    }
+                    else
+                    {
+                        score = 0;
+                    }
+                }
+                else
+                {
+                    score = 0;
+                }
+                grpSkor.Text = "Score: " + score.ToString();
+            }
+
+            if (rdoPersegi.Checked)
+            {
+                drawRectangle = true;
+                showNIM = false;
+                segiPoints = null;
+            }
+            else if (rdoSegi.Checked)
+            {
+                drawRectangle = false;
+                showNIM = false;
+                segiPoints = CalculateOctagonPoints(rectX, rectY, 70);
+            }
+            else if (rdoNIM.Checked)
+            {
+                drawRectangle = false;
+                showNIM = true;
+                segiPoints = null;
+            }
+
+            allowPaint = true;
+            pnlBlackboard.Refresh();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            myGraphics.Clear(pnlBlackboard.BackColor);
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myGraphics.Dispose();
+            myPen.Dispose();
+        }
+        private void rdoPutih_CheckedChanged(object sender, EventArgs e)
+        {
+            myPen.Color = rdoPutih.ForeColor;
+        }
+        private void rdoBiru_CheckedChanged(object sender, EventArgs e)
+        {
+            myPen.Color = rdoBiru.ForeColor;
+        }
+        private void rdoMerah_CheckedChanged(object sender, EventArgs e)
+        {
+            myPen.Color = rdoMerah.ForeColor;
+        }
+        private void rdoKuning_CheckedChanged(object sender, EventArgs e)
+        {
+            myPen.Color = rdoKuning.ForeColor;
+        }
+
+        private void pnlBlackboard_Paint(object sender, PaintEventArgs e)
+        {
+            if (allowPaint)
+            {
+                if (showNIM && rdoNIM.Checked) 
+                {
+                    string nimText = "22.60.0159";
+                    Font nimFont = new Font(FontFamily.GenericSansSerif, 15, FontStyle.Bold);
+                    SizeF nimSize = e.Graphics.MeasureString(nimText, nimFont);
+                    e.Graphics.DrawString(nimText, nimFont, myPen.Brush, rectX, rectY);
+                }
+                else if (segiPoints != null && rdoSegi.Checked)
+                {
+                    e.Graphics.DrawPolygon(myPen, segiPoints);
+                }
+                else if (drawRectangle && rdoPersegi.Checked) 
+                {
+                    e.Graphics.DrawRectangle(myPen, rectX, rectY, rectWidth, rectHeight);
+                }
+            }
+        }
+
+        private void pnlBlackboard_CheckedChanged(object sender, EventArgs e)
+        {
+            allowPaint = false; 
+        }
+
+            private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Enabled = false;
+            }
+            else
+            {
+                checkBox2.Enabled = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                Random random = new Random();
+                generatedNumber = random.Next(1, 4); 
+            }
+            else
+            {
+                generatedNumber = 0;
+                score = 0; 
+                grpSkor.Text = "Score: ";
+            }
+        }
+
+        private void rdoPersegi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoPersegi.Checked)
+            {
+                rectX = 50;
+                rectY = 50;
+                rectWidth = 70;
+                rectHeight = 70;
+
+                Random random = new Random();
+                int randomNumber = random.Next(1, 4);
+
+                if (randomNumber == 1)
+                {
+                    drawRectangle = true;
+                    segiPoints = null; 
+                    showNIM = false; 
+                    pnlBlackboard.Refresh();
+                }
+                else
+                {
+                    drawRectangle = false;
+                    pnlBlackboard.Refresh();
+                }
+            }
+        }
+
+        private void rdoNIM_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoNIM.Checked)
+            {
+                rectX = 50;
+                rectY = 90;
+
+                Random random = new Random();
+                int randomNumber = random.Next(1, 4);
+
+                if (randomNumber == 3)
+                {
+                    drawRectangle = false; // Tambahkan ini untuk menghapus drawRectangle
+                    segiPoints = null; // Tambahkan ini untuk menghapus segiPoints
+                    showNIM = true;
+                    pnlBlackboard.Refresh();
+                }
+                else
+                {
+                    showNIM = false;
+                    pnlBlackboard.Refresh();
+                }
+            }
+        }
+
+        private void rdoSegi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoSegi.Checked)
+            {
+                rectX = 50;
+                rectY = 50;
+                int rectSize = 70;
+
+                Random random = new Random();
+                int randomNumber = random.Next(1, 4);
+
+                if (randomNumber == 2)
+                {
+                    drawRectangle = false; 
+                    showNIM = false; 
+                    segiPoints = CalculateOctagonPoints(rectX, rectY, rectSize);
+                    pnlBlackboard.Refresh();
+                }
+                else
+                {
+                    segiPoints = null;
+                    pnlBlackboard.Refresh();
+                }
+                pnlBlackboard.Refresh();
+            }
+        }
+
+        private Point[] CalculateOctagonPoints(int rectX, int rectY, int rectSize)
+        {
+            Point[] points = new Point[8];
+            int halfSize = rectSize / 2;
+            int quarterSize = rectSize / 4;
+
+            points[0] = new Point(rectX + quarterSize, rectY);
+            points[1] = new Point(rectX + rectSize - quarterSize, rectY);
+            points[2] = new Point(rectX + rectSize, rectY + quarterSize);
+            points[3] = new Point(rectX + rectSize, rectY + rectSize - quarterSize);
+            points[4] = new Point(rectX + rectSize - quarterSize, rectY + rectSize);
+            points[5] = new Point(rectX + quarterSize, rectY + rectSize);
+            points[6] = new Point(rectX, rectY + rectSize - quarterSize);
+            points[7] = new Point(rectX, rectY + quarterSize);
+
+            return points;
+        }
+
+        private void grpSkor_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpBangun_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpWarna_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
